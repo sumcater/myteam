e7a8aae46ea49d9488bd2f8dbf3ca88bd6aa311c
1192439205@qq.com <1192439205@qq.com>
2019-07-20 17:13:21 +0800

    init

---
+++ src/assets/style/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssmin = require("gulp-cssmin");
+
+gulp.task("compile", function() {
+  return gulp
+    .src("./src/*.scss")
+    .pipe(sass.sync())
+    .pipe(
+      autoprefixer({
+        browsers: ["ie > 9", "last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(cssmin())
+    .pipe(gulp.dest("./lib"));
+});
