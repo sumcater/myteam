456ff05869e215197ffd904f48ea4d28d36ab8d8
1192439205@qq.com <1192439205@qq.com>
2019-08-06 16:18:43 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import form from '@/view/form'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: "/",
+      redirect:"/form"
+    },
+    {
+      path: "HelloWorld",
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: form
+    },
   ]
 })
