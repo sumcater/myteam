b38151706e9e77af5f07dbbd680a8a82e2b88a88
乐媛 <1192439205@qq.com>
2019-10-10 16:03:15 +0800

    add

---
+++ src/components/vmodel.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<!--  使用时有v-model属性，value默认值‘child’ 不会被渲染，而是v-model绑定的parent值被渲染，这儿value会被赋值为v-model绑定的parent的值。 -->
+		<p>{{value}}</p>
+		<button @click="changeValue">在子组件内修改v-model值，并且动态响应给父组件</button>
+	</div>
+</template> 
+<script>
+export default {
+	props: {
+		value: {
+			// 必须要使用value
+			default: "child"
+		}
+	},
+	methods: {
+		changeValue() {
+			let newValue = "2";
+			this.$emit("input", newValue); // 这儿必须用input 发送数据，发送的数据会被父级v-model=“parent”接收到，再被value=parent传回来。
+		}
+	}
+};
+</script>
--- src/views/dynamicQuery/dynamicQueryExecuteConfig.vue
+++ src/views/dynamicQuery/dynamicQueryExecuteConfig.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    dynamicQueryExecuteConfig
-  </div>
+	<div>
+		dynamicQueryExecuteConfig
+		<br />vue数据双向绑定
+		<br />
+		<vmodel v-model="value"></vmodel>
+		<vmodel2 v-model="parent"></vmodel2>
+	</div>
 </template>
 <script>
+import vmodel from "@/components/vmodel";
+import vmodel2 from "@/components/vmodel2";
 export default {
-  
-}
+	components: { vmodel, vmodel2 },
+	data() {
+		return {
+			value: 12,
+			parent: 11
+		};
+	}
+};
 </script>
