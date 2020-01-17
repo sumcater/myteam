62cef97bb3f2039785f2026c545df628b05a052b
乐媛 <1192439205@qq.com>
2020-01-17 11:33:45 +0800

    add

---
+++ src/assets/study/regExp.js
@@ -0,0 +1,14 @@
+// 正则
+// RegExp 只能处理字符串 
+var str = 'good good '
+var reg = /\d+/
+reg.test(str) //false
+
+str = '2019-01-28'
+reg.exec(str) // [ '2019', index: 0, input: '2019-01-28', groups: undefined ]
+
+// 编写正则表达式
+// 字面量方式
+var reg = /\d+/
+// 构造函数 两个参数 元字符和修饰符
+var reg = new RegExp('\\d+')
