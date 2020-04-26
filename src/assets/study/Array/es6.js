c80b979a2f51dac57be2d55f48263b5f04465da9
乐媛 <1192439205@qq.com>
2020-04-26 16:05:01 +0800

    add

--- src/assets/study/Array/es6.js
+++ src/assets/study/Array/es6.js
@@ -1,21 +1,27 @@
 //log
 let aa = 1;
-let log = function() {
+let log = function () {
   return console.log(...arguments);
 };
 log(aa, "888");
 
-let arr = ["aa", "ss",''];
-let arr2 = [{ label: "aa", value: 1 }, { label: "", value: 2 }, { label: "ss", value: 3 }, { label: "dd", value: 4 }];
-let ss=arr2.map(n => {
-  if (arr.includes(n.label)) return n.value
-}).filter(n => n!=undefined);
+let arr = ["aa", "ss", ''];
+let arr2 = [{ label: "aa", value: 1 }, { label: "sss", value: 2 }, { label: "ss", value: 3 }, { label: "", value: 4 }];
+// let ss=arr2.map(n => {
+//   if (arr.includes(n.label)) return n.value
+// }).filter(n => n!=undefined);
+let ss = arr2.reduce((m, n) => {
+  if (arr.includes(n.label)) {
+    m.push(n.value)
+  }
+  return m
+}, [])
 console.log(ss);
 
 let obj = {
-  a:"",
-  b:"",
-  c:"",
+  a: "",
+  b: "",
+  c: "",
 }
 // let z1 = []
 console.log([obj]);
--- src/assets/study/promise/index.js
+++ src/assets/study/promise/index.js
@@ -1,9 +1,20 @@
-const p1 = new Promise(function(resolve, reject) {
+const p1 = new Promise(function (resolve, reject) {
   setTimeout(() => reject(new Error("fail22")), 3000);
 });
 
-const p2 = new Promise(function(resolve, reject) {
+const p2 = new Promise(function (resolve, reject) {
   setTimeout(() => resolve(p1), 1000);
 });
 
 p2.then(result => console.log(result)).catch(error => console.log(error));
+
+let p = new Promise((m, n) => {
+  setTimeout(() => {
+    .2 > 0.5 ? m('success') : n('fail');
+  }, 1000)
+});
+p.then((res) => {
+  console.log(res);
+}, (e) => {
+  console.log(e);
+});
