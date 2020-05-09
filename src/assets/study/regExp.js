a378487d87bfcabc530a04c23aa0987efc895e44
乐媛 <1192439205@qq.com>
2020-05-09 16:49:57 +0800

    add replace regexp

--- src/assets/study/regExp.js
+++ src/assets/study/regExp.js
@@ -12,3 +12,11 @@ reg.exec(str) // [ '2019', index: 0, input: '2019-01-28', groups: undefined ]
 var reg = /\d+/
 // 构造函数 两个参数 元字符和修饰符
 var reg = new RegExp('\\d+')
+
+// 全局替换
+let str = "<video class ... <video "
+let str1 = str.replace(
+    new RegExp("<video ", "g"),
+    '<video controlslist="nodownload" '
+);
+console.log(str1);
