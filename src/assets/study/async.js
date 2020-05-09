cac8fc965c72d184b368e352fff5966218b42d38
乐媛 <1192439205@qq.com>
2020-05-09 16:44:36 +0800

    add async

---
+++ src/assets/study/async.js
@@ -0,0 +1,105 @@
+
+// https://segmentfault.com/a/1190000007535316
+async function test() {
+    let a = 4
+    // return "hello test"
+}
+const res = test()
+console.log(res);
+
+test().then(v => {
+    console.log(v);
+
+})
+
+function getSomething() {
+    return "something";
+}
+
+async function testAsync() {
+    return Promise.resolve("hello async");
+}
+
+async function test() {
+    const v1 = await getSomething();
+    const v2 = await testAsync();
+    console.log(v1, v2);
+}
+
+test();
+
+function takeLongTime() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("long_time_value"), 1000);
+    });
+}
+
+takeLongTime().then(v => {
+    console.log("got", v);
+});
+
+function takeLongTime() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("long_time_value"), 1000);
+    });
+}
+
+async function test() {
+    const v = await takeLongTime();
+    console.log(v);
+}
+
+test();
+
+///////////////
+/**
+ * 传入参数 n，表示这个函数执行的时间（毫秒）
+ * 执行的结果是 n + 200，这个值将用于下一步骤
+ */
+function takeLongTime(n) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(n + 200), n);
+    });
+}
+
+function step1(n) {
+    console.log(`step1 with ${n}`);
+    return takeLongTime(n);
+}
+
+function step2(n) {
+    console.log(`step2 with ${n}`);
+    return takeLongTime(n);
+}
+
+function step3(n) {
+    console.log(`step3 with ${n}`);
+    return takeLongTime(n);
+}
+
+function doIt() {
+    console.time("doIt");
+    const time1 = 300;
+    step1(time1)
+        .then(time2 => step2(time2))
+        .then(time3 => step3(time3))
+        .then(result => {
+            console.log(`result is ${result}`);
+            console.timeEnd("doIt");
+        });
+}
+
+doIt();
+
+
+async function doIt() {
+    console.time("doIt");
+    const time1 = 300;
+    const time2 = await step1(time1);
+    const time3 = await step2(time2);
+    const result = await step3(time3);
+    console.log(`result is ${result}`);
+    console.timeEnd("doIt");
+}
+
+doIt();
